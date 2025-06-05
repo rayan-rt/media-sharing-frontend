e2de6a71698dfdef8918d899c7631b0449367c27
rayan-rt <tanzeemrayan2@gmail.com>
2025-06-05 23:22:12 +0500

    integrate backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "http://127.0.0.1:5000",
+      "/api/v1": "https://media-sharing-backend.vercel.app/",
     },
   },
   plugins: [react()],
